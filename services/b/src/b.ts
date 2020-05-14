42ed29cccbc492e526890fd742a502ddbc7d9f86
Christoph Gysin <christoph.gysin@gmail.com>
2020-05-14 10:55:56 +0300

    add YAML example

--- services/b/src/b.ts
+++ services/b/src/b.ts
@@ -1,9 +1,11 @@
+import data from '../data.yml';
 import greeter from '../../../src';
 
 export const handler = async (event: any) => {
   console.log(JSON.stringify(event, null, 2));
 
   console.log('lib says:', greeter())
+  console.log('data:', JSON.stringify(data));
 
   return {
     statusCode: 200,
